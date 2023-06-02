2590f5228bc69aa6c4636f5df6504d81fc31c777
Prajwal264 <prajwalp264@gmail.com>
2023-06-02 22:13:16 +0530

    fix: update sidebar

--- apps/web/src/components/community/community-sidebar/community-sidebar-item/community-sidebar-item.tsx
+++ apps/web/src/components/community/community-sidebar/community-sidebar-item/community-sidebar-item.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import styles from './community-sidebar-item.module.scss'
 
 function CommunitySidebarItem() {
   return (
-    <div></div>
+    <div className={styles.communitySidebarItem}>
+      <img src="https://res.cloudinary.com/frond/image/upload/dpr_2.0,w_144,f_auto/w5cx4rpmnhmzefbp1qqo" alt="asd" />
+    </div>
   )
 }
 
--- apps/web/src/components/community/community-sidebar/community-sidebar.tsx
+++ apps/web/src/components/community/community-sidebar/community-sidebar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './community-sidebar.module.scss';
+import CommunitySidebarItem from './community-sidebar-item/community-sidebar-item';
 
 function CommunitySidebar() {
   return (
     <div className={styles.communitySidebar}>
-      Sidebar
+      <CommunitySidebarItem />
     </div>
   )
 }
